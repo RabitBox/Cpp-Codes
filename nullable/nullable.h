8e1e2cf3e9f74676bdf90032f48e1048d006b8e9
Gai Takakura <gixa.aturakka+git@gmail.com>
2019-07-21 00:22:59 +0900

    inline削除
    C++の仕様を確認した結果、class内で処理を実装したら自動的にinline化すると判明したため。

--- nullable/nullable.h
+++ nullable/nullable.h
@@ -25,15 +25,15 @@ public:
 	//--------------------------------------------------
 	// getter
 	// Cast to bool type.
-	inline explicit operator bool() const { return this->has_value_; }
+	explicit operator bool() const { return this->has_value_; }
 	// Cast to T type.
-	inline explicit operator T() const { if (!this->has_value_) std::runtime_error("value is none."); return this->value_; }
+	explicit operator T() const { if (!this->has_value_) std::runtime_error("value is none."); return this->value_; }
 
 	// setter
 	// Set null.
-	inline nullable<T>& operator=(const std::nullptr_t npt) { has_value_ = false; return *this; }
+	nullable<T>& operator=(const std::nullptr_t npt) { has_value_ = false; return *this; }
 	// Set T type value.
-	inline nullable<T>& operator=(const T value) { value_ = value; has_value_ = true; return *this; }
+	nullable<T>& operator=(const T value) { value_ = value; has_value_ = true; return *this; }
 	// Set nullable<T> type value.
-	inline nullable<T>& operator=(const nullable<T>& n_value) { value_ = n_value.value_; has_value_ = n_value.has_value_; return *this; }
+	nullable<T>& operator=(const nullable<T>& n_value) { value_ = n_value.value_; has_value_ = n_value.has_value_; return *this; }
 };
